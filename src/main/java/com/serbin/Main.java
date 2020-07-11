096d4b94d0e1e3b8727249152ff9540487503fb0
aserbin <alexander.serbin@hhstechgroup.com>
2020-07-11 16:01:14 -0700

    initial commit

---
+++ src/main/java/com/serbin/Main.java
@@ -0,0 +1,11 @@
+package com.serbin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Hello world");
+        }
+    }
+
+}
